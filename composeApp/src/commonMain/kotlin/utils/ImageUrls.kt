42fd20d8aed7268d292938fd7bf52faed392283a
Jason Toms <jasonctoms@gmail.com>
2024-10-10 18:26:53 +0200

    Upload more images

--- composeApp/src/commonMain/kotlin/utils/ImageUrls.kt
+++ composeApp/src/commonMain/kotlin/utils/ImageUrls.kt
@@ -1,5 +1,11 @@
 package utils
 
 enum class ImageUrls(val url: String) {
-    PROFILE("https://raw.githubusercontent.com/jasonctoms/JasonToms/main/images/profile.jpg"),
+    PROFILE("https://raw.githubusercontent.com/jasonctoms/JasonToms/main/images/profile.webp"),
+    SOGNEFJORD("https://raw.githubusercontent.com/jasonctoms/JasonToms/main/images/sognefjord.webp"),
+    STELLA_JASON_ULRIKEN("https://raw.githubusercontent.com/jasonctoms/JasonToms/main/images/meandstella.webp"),
+    STORM_AND_LUPIN("https://raw.githubusercontent.com/jasonctoms/JasonToms/main/images/stormandlupin.webp"),
+    STORM_IN_BOX("https://raw.githubusercontent.com/jasonctoms/JasonToms/main/images/stormbox.webp"),
+    BROOMY_IN_CHAIR("https://raw.githubusercontent.com/jasonctoms/JasonToms/main/images/broomychair.webp"),
+    GREY_THING_SOON("https://raw.githubusercontent.com/jasonctoms/JasonToms/main/images/greythingsoon.webp"),
 }
